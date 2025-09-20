c86818263ade621db29c061f8c0609a9375e6d98
bunnie <bunnie@kosagi.com>
2025-09-20 22:31:28 +0800

    fixups for baochip verilator environment

--- tests/src/aes.rs
+++ tests/src/aes.rs
@@ -1387,6 +1387,7 @@ macro_rules! define_aes_impl {
             inner: $name,
         }
 
+        #[allow(dead_code)]
         impl $name_dec {
             #[inline(always)]
             pub(crate) fn get_dec_backend(&self) -> $name_back_dec<'_> { self.inner.get_dec_backend() }
